1c3964c14e598fd5658cfa46d6894136ec3a79a4
bornikfik <viktorekskarda@gmail.com>
2023-07-29 19:12:38 +0200

    routing (not done)

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
   import TailwindCss from "./TailwindCSS.svelte";
   import Login from "./lib/pagestuff/Login.svelte";
+  import { Link, Route, Router, navigate } from "svelte-routing";
 </script>
 
 <main>
   <TailwindCss />
-  <Login />
+  <Router url="">
+    <Route path="/" />
+    <Route path="/login"><Login /></Route>
+  </Router>
 </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,5 @@ export default {
   variants: {
     extend: {},
   },
-  darkMode: false,
+  darkMode: "media",
 };
